85df744c396e3022f43dad0230896fe8d7aa2be0
CalvinPVIII <calvinp003@gmail.com>
2022-12-19 19:51:59 -0800

    add start for front end logic

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Conditions from "./src/models/Conditions";
 
-import { Window, App, Text } from "proton-native";
+import { Window, App, Text, Button } from "proton-native";
 
 export default function Main() {
   const chosenConditions = new Conditions();
+  const [currentState, setCurrentState] = useState("home");
   console.log();
+
+  const render = () => {
+    switch (currentState) {
+      case "weapon":
+        return <Text>Weapon</Text>;
+      default:
+        <Text>Home</Text>;
+    }
+  };
+
   return (
     <App>
       <Window style={{ width: 800, height: 600 }}>
-        <Text style={{ fontSize: "100px" }}>HELLO</Text>
+        <Text style={{ fontSize: "20px" }}>DAR CONDITION MAKER</Text>
+        <Button onPress={() => setCurrentState("weapon")} title="Switch" />
+        {render()}
       </Window>
     </App>
   );
